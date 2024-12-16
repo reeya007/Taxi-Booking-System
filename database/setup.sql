c807a37c74575a5f9ff15a896866d0c47dc36c85
Reeya Karki <reeyakrk@gmail.com>
2024-12-16 21:52:38 +0545

    Updated Sql Query

--- database/setup.sql
+++ database/setup.sql
@@ -4,29 +4,29 @@ USE TaxiBooking;
 
 CREATE TABLE Customers (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255),
-    address VARCHAR(255),
-    phone_number VARCHAR(15),
-    email VARCHAR(255) UNIQUE,
-    password VARCHAR(255)
+    name VARCHAR(255) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(15) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE Drivers (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255),
-    license_number VARCHAR(15),
-    phone_number VARCHAR(15),
-    is_available BOOLEAN DEFAULT TRUE
-    email VARCHAR(255) UNIQUE,
-    password VARCHAR(255)
+    name VARCHAR(255) NOT NULL,
+    license_number VARCHAR(15) NOT NULL,
+    phone_number VARCHAR(15) NOT NULL,
+    is_available BOOLEAN DEFAULT TRUE,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE Bookings (
     id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
-    pickup_location VARCHAR(255),
-    dropoff_location VARCHAR(255),
-    trip_date DATETIME,
+    pickup_location VARCHAR(255) NOT NULL,
+    dropoff_location VARCHAR(255) NOT NULL,
+    trip_date DATETIME NOT NULL,
     driver_id INT DEFAULT NULL,
     is_completed BOOLEAN DEFAULT FALSE,
     FOREIGN KEY (customer_id) REFERENCES Customers(id),
